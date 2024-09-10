0bd732bc665ced056be710ee4317fca2c51875bb
Natan <nr020775@gmail.com>
2024-09-10 20:10:26 -0300

    first commit

---
+++ tela_inicial/javascript/navbar.js
@@ -0,0 +1,42 @@
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+    
+    var navbar = document.querySelector('#navbar')
+    var logo = document.querySelector('#logo')
+    
+    if(window.scrollY > 0){
+        logo.style.transform = 'scale(0.6)'
+        
+        logo.addEventListener('mouseover', function(){ 
+            this.style.transform = 'scale(0.72)'
+        })
+        
+        
+        logo.addEventListener('mouseout', function(){
+            this.style.transform = 'scale(0.6)'
+        })
+        
+    } else {
+        logo.style.transform = 'scale(1)  translateX(0)'
+        navbar.style.transform = 'scale(1) translate(0)'
+        
+        logo.addEventListener('mouseover', function(){
+            this.style.transform = 'scale(1.2)'
+        })
+        
+        logo.addEventListener('mouseout', function(){
+            this.style.transform = 'scale(1)'
+        })
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    var botao = document.querySelector("#botao")
+    botao.classList.toggle('btnrolagem', window.scrollY > 0)
+
+})
+
